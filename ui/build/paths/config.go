f0821f42957411be726c6b65d8043a7f527d6263
Ralf Luther <luther.ralf@gmail.com>
2020-12-05 12:55:50 +0100

    soong: whitelist the usage of Python 3.9
    Change-Id: I3dac2f690cf5165d3bf1937acaae1496d6aba45f

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -109,6 +109,7 @@ var Configuration = map[string]PathConfig{
 	"python3.6":Allowed,
 	"python3.7":Allowed,
 	"python3.8":Allowed,
+	"python3.9":Allowed,
         "realpath": Allowed,
 	"repo":     Allowed,
 	"rsync":    Allowed,
